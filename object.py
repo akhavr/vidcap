28b08589db0066dada2e1f9bc121eba439834caf
Andriy Khavryuchenko <akhavr@gmail.com>
2020-05-11 12:21:22 +0100

    Add headless option to the script

--- object.py
+++ object.py
@@ -28,6 +28,8 @@ ap.add_argument("--codec", type=str, default="MJPG",
 	help="codec of output video")
 ap.add_argument("-f", "--fps", type=int, default=20,
 	help="FPS of output video")
+ap.add_argument('--headless', dest='headless', action='store_true', help='Run headless')
+ap.set_defaults(headless=False)
 args = vars(ap.parse_args())
 
 # initialize the list of class labels MobileNet SSD was trained to
@@ -55,87 +57,97 @@ kcw = KeyClipWriter(bufSize=args["buffer_size"])
 consecFrames = 0 #number of frames with no motion
 
 # loop over the frames from the video stream
-while True:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
-    updateConsecFrames = True
+try:
+    while True:
+        # grab the frame from the threaded video stream and resize it
+        # to have a maximum width of 400 pixels
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        updateConsecFrames = True
 
-    # update the key frame clip buffer
-    kcw.update(frame)
+        # update the key frame clip buffer
+        kcw.update(frame)
 
-    # grab the frame dimensions and convert it to a blob
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-                                 0.007843, (300, 300), 127.5)
-    # pass the blob through the network and obtain the detections and
-    # predictions
-    net.setInput(blob)
-    detections = net.forward()
+        # grab the frame dimensions and convert it to a blob
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+                                     0.007843, (300, 300), 127.5)
+        # pass the blob through the network and obtain the detections and
+        # predictions
+        net.setInput(blob)
+        detections = net.forward()
 
-    # loop over the detections
-    for i in np.arange(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[0, 0, i, 2]
-        
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
-        if confidence > args["confidence"]:
-            # extract the index of the class label from the
-            # `detections`, then compute the (x, y)-coordinates of
-            # the bounding box for the object
-            idx = int(detections[0, 0, i, 1])
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+        print(detections.shape[2], 'detected')
+        # loop over the detections
+        for i in np.arange(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with
+            # the prediction
+            confidence = detections[0, 0, i, 2]
 
-            # draw the timestamp
-            timestamp = datetime.datetime.now()
-            cv2.putText(frame, timestamp.strftime("%Y.%m.%d %H:%M:%S"), (5, 15),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[0], 2)
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+            if confidence > args["confidence"]:
+                # extract the index of the class label from the
+                # `detections`, then compute the (x, y)-coordinates of
+                # the bounding box for the object
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                # draw the timestamp
+                timestamp = datetime.datetime.now()
+                cv2.putText(frame, timestamp.strftime("%Y.%m.%d %H:%M:%S"), (5, 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[0], 2)
+
+                # draw the prediction on the frame
+                label = "{}: {:.2f}%".format(CLASSES[idx],
+                                             confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                print(timestamp, CLASSES[idx], confidence*100)
+
+                consecFrames = 0
+                # if we are not already recording, start recording
+                if not kcw.recording:
+                    p = "{}/{}.avi".format(args["output"],
+                                           timestamp.strftime("%Y%m%d-%H%M%S"))
+                    kcw.start(p, cv2.VideoWriter_fourcc(*args["codec"]),
+                              args["fps"])
+
+        # otherwise, no action has taken place in this frame, so
+        # increment the number of consecutive frames that contain
+        # no action
+        if updateConsecFrames:
+            consecFrames += 1
+
+        # if we are recording and reached a threshold on consecutive
+        # number of frames with no action, stop recording the clip
+        if kcw.recording and consecFrames == args["buffer_size"]:
+            kcw.finish()
+
+        if not args['headless']:
+            # show the output frame
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
             
-            # draw the prediction on the frame
-            label = "{}: {:.2f}%".format(CLASSES[idx],
-                                         confidence * 100)
-            cv2.rectangle(frame, (startX, startY), (endX, endY),
-                          COLORS[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+        # update the FPS counter
+        fps.update()
+except:
+    pass
 
-            consecFrames = 0
-            # if we are not already recording, start recording
-            if not kcw.recording:
-                p = "{}/{}.avi".format(args["output"],
-                                       timestamp.strftime("%Y%m%d-%H%M%S"))
-                kcw.start(p, cv2.VideoWriter_fourcc(*args["codec"]),
-                          args["fps"])
-
-    # otherwise, no action has taken place in this frame, so
-    # increment the number of consecutive frames that contain
-    # no action
-    if updateConsecFrames:
-        consecFrames += 1
-
-    # if we are recording and reached a threshold on consecutive
-    # number of frames with no action, stop recording the clip
-    if kcw.recording and consecFrames == args["buffer_size"]:
-        kcw.finish()
-        
-    # show the output frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-    # update the FPS counter
-    fps.update()
-    
+kcw.finish()
 # stop the timer and display FPS information
 fps.stop()
 print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
